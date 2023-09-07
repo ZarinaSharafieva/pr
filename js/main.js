71ce35c3f98ea8c4231e8056bea0f55eaa3a31d3
SharafievaZarina <sharafievazarina@gmail.com>
2023-09-07 16:12:28 +0300

    practica 2 pop1

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// let a ='text';
+// let s =323;
+// console.log('vgmkg');
+// console.log(a);
+
+// let x = 100;
+// let y = 100;
+// console.log( y + x )
+// console.log('---------------')
+
+// alert(a + 'text')
+// let what = prompt('сколько?')
+// console.log(Number(what))
+// let sure = confirm('уверен')
+
+
+let gorod = prompt('Укажите название вашего города')
+let data = prompt('год образования вашего города')
+
+let nasel = prompt('население')
+Number(nasel)
+let voz = 2024- Number(data)
+console.log('Городу '+ gorod + ' исполнилось ' + voz + ' лет с момента его образования. Население - ' + nasel +' человек')
